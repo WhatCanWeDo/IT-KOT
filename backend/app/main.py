8af530346bbcbbc1705a8af2452cfba84eba0334
not-solemn-leader <14yearevolution@gmail.com>
2021-04-10 15:48:07 +0300

    Added handler

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from .routers import query_router
 app = FastAPI()
-#app = FastAPI(root_path='/api/v1')
 
 origins = [
 
--- backend/app/routers/query_router.py
+++ backend/app/routers/query_router.py
@@ -5,32 +5,15 @@ import logging
 from ..speech2order import speech2order
 
 router = APIRouter()
+prices = [250, 170, 195, 205, 120, 450, 370, 240, 200, 450, 200]
 
 
-async def send_request_to_kitchen(request, cafe_id):
-    async with httpx.AsyncClient() as client:
-        response = await client.post('address/' + cafe_id + '/order', data=request)
-    json = response.json()
-    if not json['success']:
-        logging.warning(f'Error with sending request to the kitchen \n Kitchen\'s response : {json}')
-
-
-@router.post('/send-speech-order')
-async def send_speech_order(request: Request):
-    if not request['speech_url']:
-        logging.warning('Missing speech_url')
-        return JSONResponse(status_code=400, content={'success': False, 'error_message': 'Expected speech url'})
-
-    json = await speech2order(speech_url=request['speech_url'])
-    await send_request_to_kitchen(json, cafe_id=request['cafe_id'])
-    return JSONResponse(status_code=200, content={'success': True})
-
-
-@router.post('/send-order')
-async def send_text(request: Request):
-    if not request['data']:
-        logging.warning('Missing json file')
-        return JSONResponse(status_code=400, content={'success': False, 'error_message': 'Expected json file'})
-
-    await send_request_to_kitchen(request['data'], cafe_id=request['cafe_id'])
-    return JSONResponse(status_code=200, content={'success': True})
+@router.post('make-order')
+async def make_order(r: Request):
+    item_ids = r['items']
+    p = 0
+    for i in item_ids:
+        print('Цена товара с id', i, '=', prices[i], 'рублей')
+        p += prices[i]
+    print('Цена заказа', p, 'рублей')
+    return JSONResponse(status_code=200)
